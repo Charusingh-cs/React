6341a584714413abdcd4e05e77243334b0705b59
Charusingh-cs <charusandhu272@gnail.com>
2026-04-24 10:28:58 +0530

    Nerdid

---
+++ Nerdid/src/components/Greeting.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Greeting(props) {
+  return (
+    <div>
+        <h1>Hello{name}</h1>
+      
+    </div>
+  )
+}
+// function Greeting(props){
+//     return {
+//     <div>
+//         </div>    }
+// }
+//filter map reduce is used in traverse
+//default props 
+
+
+export default Greeting
